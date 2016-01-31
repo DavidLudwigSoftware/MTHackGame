7825c00ef49c8caa6477f521948b9c229388b735
Joshua Taylor <TJoshuacraig@gmail.com>
2016-01-31 06:13:07 -0600

    updated

--- gui/entities/player.py
+++ gui/entities/player.py
@@ -14,25 +14,18 @@ class Player(Entity):
     FacingDown    = 10
     FacingNeutral = 20
 
+
     def __init__(self, screen, world, player = 0, facing = FacingLeft+FacingNeutral):
 
         self.__player = player
-        self.__rect = [0, 0, 70, 70]
+        self.__rect = [0, 0, 50, 50]
 
         self.__facing = facing
-
-        name = "res/sprites/players/p" + str(player + 1) + "_"
-
-        self.__sprites = [
-            pygame.image.load(name + "rgun.png")
-        ]
 
         super(Player, self).__init__(screen, world, self.__rect)
 
         self.__MHP = 100
         self.__HP = self.__MHP
-
-        
 
 
     def update(self):
@@ -50,7 +43,7 @@ class Player(Entity):
 
     def render(self):
 
-        self.screen().surface().blit(self.__sprites[0], (self.x(), self.y()))
+        pygame.draw.rect(self.screen().surface(), (0xff, 0x00, 0x00), self.rect())
 
     def facing(self):
         return self.__facing
